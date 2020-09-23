02ed295d974634c4d81e86ccf545af5a2ca2fcce
Hameln13 <68385230+Hameln13@users.noreply.github.com>
2020-09-24 01:42:47 +0900

    Add files via upload

---
+++ sent.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    <div class="main">
+        <div class="thx_message">THX</div>
+        <div>名前</div>
+        <?php echo $_POST[name]; ?>
+</body>
+</html>
